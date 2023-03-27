499cd98f6db7aa91cfa466666d88a7cc007945fc
shreyaskhamkar <108938062+shreyaskhamkar@users.noreply.github.com>
2023-03-27 15:22:38 +0530

    Added Radio button Events and ToolTip properties (#5)
    Co-authored-by: shreyaskhamkae <khamk@SHREYAS-PC>

---
+++ RadioButtons/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RadioButtons
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            rbMaybe.IsChecked = true;
+        }
+
+
+        private void YesChecked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Thank you!");
+        }
+
+
+        private void NoChecked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Please say yes");
+        }
+        private void MaybeChecked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Please say yes");
+        }
+    }
+}
