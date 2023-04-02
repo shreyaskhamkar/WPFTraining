a897aa679759a81cedf38328c5d0a2939b4e8361
shreyaskhamkar <108938062+shreyaskhamkar@users.noreply.github.com>
2023-04-02 09:30:25 +0530

    Update App.xaml.cs

--- DataBinding/App.xaml.cs
+++ DataBinding/App.xaml.cs
@@ -8,9 +8,7 @@ using System.Windows;
 
 namespace DataBinding
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+  
     public partial class App : Application
     {
     }
